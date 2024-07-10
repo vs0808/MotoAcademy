99578b562be28a7a0e0810af96f40370ec848e49
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-07-10 18:53:03 -0400

    aula 1 de javascript

---
+++ javascrpit/aula_js.js
@@ -0,0 +1,68 @@
+/*
+const number = 1; // number
+const string = "texto"; //string
+const booleano = true; // boolean true or false
+const nulo = null;
+const undefined = undefined;
+
+const objeto = {
+  nome: "Alan Barroncas",
+  idade: 22,
+};
+
+objeto.nome = "Alex";
+const array = ["teste", 212130, true];
+
+const soma = 1 + 1;
+const subtracaoo = 3 - 1;
+const multiplicacao = 2 * 1;
+const divisao = 4 / 2;
+
+
+const valorConstante = "constante";
+const array = [];
+array.push(20);
+
+const objeto = {
+  aluno: "Alan",
+};
+objeto.aluno = "Alan";
+console.log(objeto);
+
+
+let variavel2 = "valor fora";
+
+function teste() {
+  let variavel2 = "valor dentro";
+  varialvel2 = "mudamos o valor dentro";
+  console.log(variavel2);
+}
+teste();
+console.log(variavel2);
+
+
+let button = "oi";
+
+function teste() {
+  teste2();
+}
+
+function teste2() {
+  variavelGlobal = "tchau";
+}
+
+teste();
+console.log(variavelGlobal);
+*/
+
+const aluno = "Alan";
+if (aluno == "Alex") {
+  console.log("Alex");
+} else {
+  console.log("Alan");
+}
+
+const numero = 29;
+if (numero == "29") {
+  console.log("ele Ã© igual a 22");
+}
