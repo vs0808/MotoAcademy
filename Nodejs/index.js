58eb40e7885c0c6aeb9887e5e83a3ddab8804c33
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-08-14 20:22:27 -0400

    NodeJS

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -4,16 +4,27 @@ const app = express();
 
 app.use(express.json())
 
+const checkLogin = (req, res, next) =>{
+
+    const logado = req.headers.authorization == "Bearer tokenValido"
+    if(logado){
+        next()
+    }else{
+        res.status(401).json({message: "Por favor verifique suas credenciais"})
+    }
+}
+
 let users = [
-    {id: 1, name: "Euclides"},
-    {id: 2, name: "Filipe"},
+    {id: 1, name: "Euclides", email: "eu@gmail.com", senha:'12345'},
+    {id: 2, name: "Filipe", email:"vc@gmail.com", senha:'12345'},
 ]
 
-app.get('/usuarios', (req, res)=> {
+app.get('/usuarios', checkLogin,(req, res)=> {
+    
     res.json(users);
 })
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', checkLogin, (req, res) => {
     console.log(req.body);
     
     const name = req.body.name;
@@ -25,7 +36,7 @@ app.post('/usuarios', (req, res) => {
 
 //http://localhost:3000/usuarios/1
 
-app.put("/usuarios/:id", (req,res)=>{
+app.put("/usuarios/:id", checkLogin, (req,res)=>{
     const id =  parseInt(req.params.id)
     const {name} = req.body
     const userIndex = users.findIndex(user=>user.id==id);
@@ -37,13 +48,23 @@ app.put("/usuarios/:id", (req,res)=>{
     }
 })
 
-app.delete('/usuarios/:id', (req,res)=>{
+app.delete('/usuarios/:id',checkLogin , (req,res)=>{
     const id = parseInt(req.params.id);
     users = users.filter(user=>user.id !== id);
     res.sendStatus(204);
 })
 
-app.get('/', (req,res) => {
+app.post('/login', checkLogin, (req,res)=>{
+    const {name, email, senha} = req.body;
+    const user = users.find(user => user.email == email && user.senha == senha)
+    if(user){
+        res.json({message: "Login bem sucedido!", token: "tokenValido"})
+    }else{
+        res.json({message:"Credenciais Inválidas"})
+    }
+})
+
+app.get('/',checkLogin , (req,res) => {
     res.send({message: "Olá mundo"})
 })
 
