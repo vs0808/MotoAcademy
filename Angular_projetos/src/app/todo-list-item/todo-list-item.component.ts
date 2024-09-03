00f7ec2c31ba06eb1ca7e4a859cc32acc990786f
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-09-02 20:31:49 -0400

    inicio do Angular

---
+++ Angular_projetos/src/app/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,11 @@
+import {TodoListItem} from './todo-list-item.component.ts';
+@Component({
+  standalone: true,
+  imports: [TodoListItem],
+  template: `
+    <ul>
+      <todo-list-item></todo-list-item>
+    </ul>
+  `,
+})
+export class TodoList {}
