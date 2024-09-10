31c5dbb3e0160ff68534a50dbcdd8a5803c40574
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-09-09 20:30:58 -0400

    Docs Angular

---
+++ Angular_projetos/src/app/componente01/componente01.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { eventNames } from 'process';
+
+@Component({
+  selector: 'app-componente01',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente01.component.html',
+  styleUrl: './componente01.component.css'
+})
+export class Componente01Component {
+  name: string = 'Vinicius'
+  name2: string = 'Claudio'
+  
+
+  alerta(){
+    alert("clicked")
+  }
+
+}
