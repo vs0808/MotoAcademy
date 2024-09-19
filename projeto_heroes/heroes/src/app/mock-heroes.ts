837190113f6fcd5828ebccda119dd82b67c8119a
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-09-18 20:29:22 -0400

    Aula 01 projeto heroes

---
+++ projeto_heroes/heroes/src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Batman' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Wonder Woman' },
+  { id: 14, name: 'Spider-Man' },
+  { id: 15, name: 'Iron Man' },
+  { id: 16, name: 'Captain America' },
+  { id: 17, name: 'Thor' },
+  { id: 18, name: 'Hulk' },
+  { id: 19, name: 'Black Widow' },
+  { id: 20, name: 'Aquaman' },
+  { id: 21, name: 'Flash' },
+  { id: 22, name: 'Green Lantern' },
+  { id: 23, name: 'Doctor Strange' },
+  { id: 24, name: 'Black Panther' },
+  { id: 25, name: 'Wolverine' },
+  { id: 26, name: 'Daredevil' },
+  { id: 27, name: 'Green Arrow' },
+  { id: 28, name: 'Cyborg' },
+  { id: 29, name: 'Scarlet Witch' },
+  { id: 30, name: 'Vision' },
+]
