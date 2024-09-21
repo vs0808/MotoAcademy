7b91c0b4ca59418932bf1918e79f2ae291727306
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-09-20 20:22:12 -0400

    Aula 2 do projeto Heroes

--- projeto_heroes/heroes/src/app/heroes/heroes.component.ts
+++ projeto_heroes/heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,20 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES
-  
+  heroes:Hero[] = []
+  selectedHero?:Hero;
+
+  constructor(private heroService: HeroService){}
+
+  ngOnInit():void{
+    this.getHeroes()
+  }
+
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero
+  }
+
+  getHeroes():void{
+    this.heroes = this.heroService.getHeroes()
+  }
 }
