aa78b045dee0cb29ec2b11311e52aab72be5f76d
Vinicius de Sousa <vinicius.souza.menezes@gmail.com>
2024-09-25 19:59:39 -0400

    aula 04 projeto heroes

--- projeto_heroes/heroes/src/app/hero.service.ts
+++ projeto_heroes/heroes/src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     this.messageService.add('HeroSerivice: listagem herois')
     return heroes;
    }
+
+  getHero(id:number): Observable<Hero>{
+    const hero = HEROES.find(hero => hero.id ==id)!;
+    this.messageService.add(`Hero Service: heroi selecionado id=${id}`)
+    return of (hero);
+  }
 }
